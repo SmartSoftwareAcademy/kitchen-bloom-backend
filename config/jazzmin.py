7818b2e974863d03e1e1ea8fe973850aab532eff
Titus Owuor <54272773+titusowuor30@users.noreply.github.com>
2025-07-11 22:43:12 +0300

    Update jazzmin.py

--- config/jazzmin.py
+++ config/jazzmin.py
@@ -367,7 +367,7 @@ JAZZMIN_UI_TWEAKS = {
     "sidebar_nav_compact_style": True,
     "sidebar_nav_legacy_style": False,
     "sidebar_nav_flat_style": False,
-    "theme": "simplex",
+    "theme": "cerulean",
     "dark_mode_theme": None,
     "button_classes": {
         "primary": "btn-primary",
